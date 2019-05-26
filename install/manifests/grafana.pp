79e674313842eeccd40ad88b34d1b9d3e1a937f6
coxifred <tusti70@google.com>
2019-05-26 17:27:45 +0200

    change installation mode

--- install/manifests/grafana.pp
+++ install/manifests/grafana.pp
@@ -1,32 +1,18 @@
 notify {'Grafana for PimpMyBeetle installation and configuration':}
 
-package {'curl':
-          ensure => present,
-        }
-
-class { 'apache': default_vhost => false }
-
-  apache::vhost { 'pimpMyBeetle':
-    servername      => 'pimpMyBeetle',
-    port            => 80,
-    docroot         => '/opt/grafana',
-    error_log_file  => 'grafana_error.log',
-    access_log_file => 'grafana_access.log',
-    directories     => [
-      {
-        path            => '/opt/grafana',
-        options         => [ 'None' ],
-        allow           => 'from All',
-        allow_override  => [ 'None' ],
-        order           => 'Allow,Deny',
-      }
-    ]
-  }->
-class {'grafana':
-  influxdb_host   => 'localhost',
-  influxdb_dbpath => '/db/pimpMyBeetle',
-  influxdb_user   => 'pimpMyBeetle',
-  influxdb_pass   => 'pimpMyBeetle',
-  influxdb_grafana_user   => 'pimpMyBeetle',
-  influxdb_grafana_pass   => 'pimpMyBeetle',
+apt::source { 'grafana':
+               location => 'https://packages.grafana.com/oss/deb',
+               repos    => 'main',
+               release  => 'stable',
 }
+
+package {'grafana':
+          ensure          => present,
+          install_options => ['--allow-unauthenticated'],
+          notify          => Service['grafana-server.service'],
+}
+
+service {'grafana-server.service':
+          ensure => 'running',
+          enable => true,
+        }
