f0299ec7c9c42b5545164a7cf8ba45322ca096b0
coxifred <tusti70@gmail.com>
2019-05-27 00:33:30 +0200

    Work

--- install/manifests/grafana.pp
+++ install/manifests/grafana.pp
@@ -13,8 +13,104 @@ package {'grafana':
           require         => Apt::Source['grafana'],
 }
 
+exec {'setcap':
+      path    => "/bin:/sbin:/usr/bin:/usr/sbin",
+      command => "setcap cap_net_bind_service+ep /usr/sbin/grafana-server",
+      require => Package['grafana'],
+     }
+
+ini_setting { 'ini_http':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'server',
+  setting => 'protocol',
+  value   => 'http',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_port':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'server',
+  setting => 'http_port',
+  value   => '80',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_auth':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'auth',
+  setting => 'disable_login_form',
+  value   => 'true',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_anoauth':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'auth.anonymous',
+  setting => 'enabled',
+  value   => 'true',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_users_org':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'users',
+  setting => 'auto_assign_org_role',
+  value   => 'Admin',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_users_view':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'users',
+  setting => 'viewers_can_edit',
+  value   => 'true',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+ini_setting { 'ini_users_editor':
+  ensure  => present,
+  path    => '/etc/grafana/grafana.ini',
+  section => 'users',
+  setting => 'editors_can_admin',
+  value   => 'true',
+  notify  => Service['grafana-server.service'],
+  require => Package['grafana'],
+}
+
+grafana_datasource { 'influxdb':
+  grafana_url      => 'http://localhost:80',
+  grafana_user     => 'admin',
+  grafana_password => 'admin',
+  grafana_api_path => '/api',
+  type             => 'influxdb',
+  organization     => 'pimpMyBeetle',
+  url              => 'http://localhost:8086',
+  user             => 'pimpMyBeetle',
+  password         => 'pimpMyBeetle',
+  database         => 'pimpMyBeetle',
+  access_mode      => 'direct',
+  is_default       => true,
+}
+
+
 service {'grafana-server.service':
           ensure  => 'running',
           enable  => true,
-          require => Package['grafana-server.service'],
+          require => [Package['grafana'],Exec['setcap']],
         }
+
+
+
+
--- install/manifests/influxDb.pp
+++ install/manifests/influxDb.pp
@@ -24,12 +24,19 @@ service {'influxdb.service':
 
 exec { "manage_influxdb_${db_name}":
       path      => '/bin:/usr/bin',
-      command   => "influx -execute 'CREATE DATABASE ${db_name}'",
+      command   => "influx -execute 'CREATE DATABASE ${db_name}' ; influx -execute 'CREATE USER pimpMyBeetle WITH PASSWORD PimpMyBeetle WITH ALL PRIVILEGES'",
       unless    => "influx -execute 'SHOW DATABASES' | grep -qs \"^${db_name}\$\"",
       onlyif    => undef,
       tries     => 3,
       try_sleep => 10,
       require   => Service['influxdb.service'],
+    }
+
+exec { "manage_influxdb_${db_name}_user":
+      path      => '/bin:/usr/bin',
+      command   => "influx -database pimpMyBeetle -execute \"CREATE USER pimpMyBeetle WITH PASSWORD \'PimpMyBeetle\' WITH ALL PRIVILEGES\"",
+      unless    => "influx -execute 'SHOW users' | grep -c pimpMyBeetle",
+      require   => Exec["manage_influxdb_${db_name}"],
     }
 
 
