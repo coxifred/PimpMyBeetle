44b53d584450961e702dceef9ff12eee623ead59
LostBaboon <domotic70@gmail.com>
2019-05-26 11:29:16 +0200

    Update influxDb.pp

--- install/manifests/influxDb.pp
+++ install/manifests/influxDb.pp
@@ -3,7 +3,7 @@ notify {'InfluxDb configuration':}
 
 apt::source { 'influxDb':
   location => 'https://repos.influxdata.com/debian',
-  repos    => 'strech stable',
-  release  => '9',
+  repos    => 'strech',
+  release  => 'stable',
 }
 
