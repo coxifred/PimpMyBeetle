a10bfb25536e28b5c18f56eec6fa7431876b588c
coxifred <tusti70@google.com>
2019-05-26 16:46:44 +0200

    work

--- install/manifests/grafana.pp
+++ install/manifests/grafana.pp
@@ -1,8 +1,32 @@
-class grafana
-{
+notify {'Grafana for PimpMyBeetle installation and configuration':}
 
+package {'curl':
+          ensure => present,
+        }
 
- notify('Grafana for PimpMyBeetle installation and configuration')
+class { 'apache': default_vhost => false }
 
-
+  apache::vhost { 'pimpMyBeetle':
+    servername      => 'pimpMyBeetle',
+    port            => 80,
+    docroot         => '/opt/grafana',
+    error_log_file  => 'grafana_error.log',
+    access_log_file => 'grafana_access.log',
+    directories     => [
+      {
+        path            => '/opt/grafana',
+        options         => [ 'None' ],
+        allow           => 'from All',
+        allow_override  => [ 'None' ],
+        order           => 'Allow,Deny',
+      }
+    ]
+  }->
+class {'grafana':
+  influxdb_host   => 'localhost',
+  influxdb_dbpath => '/db/pimpMyBeetle',
+  influxdb_user   => 'pimpMyBeetle',
+  influxdb_pass   => 'pimpMyBeetle',
+  influxdb_grafana_user   => 'pimpMyBeetle',
+  influxdb_grafana_pass   => 'pimpMyBeetle',
 }
--- install/manifests/init.pp
+++
@@ -1,5 +0,0 @@
-# This is the first Puppet Manifest for PimpMyBeetle
-
-
-notify {'Puppet for PimpMyBeetle installation and configuration':}
-
